3e330c56a8472a1f3de112ebf5d4acf140153862
Md Razu Ahammed Molla <razuahammed@Mds-iMac.local>
2018-05-25 17:23:43 +0530

    new adjamnet

--- routes/index.js
+++ routes/index.js
@@ -74,9 +74,9 @@ router.post('/', function(req, res, next) {
         console.log("You already got the coupon")
 
       //var message = req.body.FirstName+", תודה על הרשמתך !! להזכירך, קוד הטבת 10% הנחה:("+result.coupon+"), למימוש חד פעמי באחת מהרשתות: גלי, סולוג, לי קופר, ניין ווסט, איזי ספיריט, אן קליין, כל נעל סנטר ושואו-אופ. עד 31.5.18, כפוף לתנאי המבצעים, להסרה השב הסר";
-        var message = req.body.FirstName+", תודה על הרשמתך  להזכירך, קוד הטבת 10% הנחה:("+result.coupon+"), למימוש חד פעמי באחת מהרשתות: גלי, סולוג, לי קופר, ניין ווסט, איזי ספיריט, אן קליין, כל נעל סנטר ושואו-אופ. עד 30.5.18, כפוף לתנאי המבצעים, להסרה השב הסר";
-
-      console.log(message);
+      //  var message = req.body.FirstName+", תודה על הרשמתך  להזכירך, קוד הטבת 10% הנחה:("+result.coupon+"), למימוש חד פעמי באחת מהרשתות: גלי, סולוג, לי קופר, ניין ווסט, איזי ספיריט, אן קליין, כל נעל סנטר ושואו-אופ. עד 30.5.18, כפוף לתנאי המבצעים, להסרה השב הסר";
+         var message =req.body.FirstName+", תודה על הרשמתך !! להזכירך, קוד הטבת 10% הנחה: ("+result.coupon+"), למימוש חד פעמי באחת מהרשתות: גלי, סולוג, לי קופר, ניין ווסט, איזי ספיריט, אן קליין ושואו-אופ. עד 30.6.18, כפוף לתנאי המבצעים, להסרה השב הסר";
+         console.log(message);
 
       var data = '<?xml version="1.0" encoding="UTF-8"?><sms><account><id>gali85</id> <password>gali713</password></account> <attributes><reference>123</reference><replyPath>0508085969</replyPath> </attributes><schedule> <relative>0</relative></schedule> <targets><cellphone reference="3542">'+userPhone+'</cellphone> </targets><data>'+message+'</data></sms>'
 
@@ -142,8 +142,19 @@ router.post('/', function(req, res, next) {
 
                         //Send sms
 
-                        var message = req.body.FirstName+", תודה על הרשמתך !! קוד הטבת 10% הנחה: ( "+couponId+" ), למימוש חד פעמי באחת מהרשתות: גלי, סולוג, לי קופר, ניין ווסט, איזי ספיריט, אן קליין, כל נעל סנטר ושואו-אופ. עד 30.5.18, כפוף לתנאי המבצעים, להסרה השב הסר";
-                        console.log("New Message"+message);
+
+
+                                request.post({url:'http://www.pages02.net/nessatltd-brill/iframeWS', form: {fn:req.body.FirstName,ln:req.body.LastName,phone:req.body.telephone,email:req.body.email,nl:"1",cpn:couponId,med:"sms",src:'קמפיין השקה'}}, function(err,httpResponse,body){
+                              	console.log("Sent to watson "+httpResponse)
+                              	//console.log(body)
+                              })
+
+                        var message= req.body.FirstName+", תודה על הרשמתך !! קוד הטבת 10% הנחה: ("+couponId+"), למימוש חד פעמי באחת מהרשתות: גלי, סולוג, לי קופר, ניין ווסט, איזי ספיריט, אן קליין ושואו-אופ. עד 30.6.18, כפוף לתנאי המבצעים, להסרה השב הסר"
+                    //  var message = req.body.FirstName+", תודה על הרשמתך !! קוד הטבת 10% הנחה: ( "+couponId+" ), למימוש חד פעמי באחת מהרשתות: גלי, סולוג, לי קופר, ניין ווסט, איזי ספיריט, אן קליין, כל נעל סנטר ושואו-אופ. עד 30.5.18, כפוף לתנאי המבצעים, להסרה השב הסר";
+                      console.log("New Message"+message);
+
+
+
 
                         var data = '<?xml version="1.0" encoding="UTF-8"?><sms><account><id>gali85</id> <password>gali713</password></account> <attributes><reference>123</reference><replyPath>0508085969</replyPath> </attributes><schedule> <relative>0</relative></schedule> <targets><cellphone reference="3542">'+userPhone+'</cellphone> </targets><data>'+message+'</data></sms>'
 
--- test.js
+++ test.js
@@ -1,7 +1,27 @@
-var request = require('request');
 
-request('http://www.google.com', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
- // console.log('body:', body); // Print the HTML for the Google homepage.
-});
+
+
+console.log("You already got the coupon")
+
+      //var message = req.body.FirstName+", תודה על הרשמתך !! להזכירך, קוד הטבת 10% הנחה:("+result.coupon+"), למימוש חד פעמי באחת מהרשתות: גלי, סולוג, לי קופר, ניין ווסט, איזי ספיריט, אן קליין, כל נעל סנטר ושואו-אופ. עד 31.5.18, כפוף לתנאי המבצעים, להסרה השב הסר";
+    //    var message = req.body.FirstName+", תודה על הרשמתך  להזכירך, קוד הטבת 10% הנחה:("+result.coupon+"), למימוש חד פעמי באחת מהרשתות: גלי, סולוג, לי קופר, ניין ווסט, איזי ספיריט, אן קליין, כל נעל סנטר ושואו-אופ. עד 30.5.18, כפוף לתנאי המבצעים, להסרה השב הסר";
+
+
+      var message =req.body.FirstName+", תודה על הרשמתך !! להזכירך, קוד הטבת 10% הנחה: ("+result.coupon+"), למימוש חד פעמי באחת מהרשתות: גלי, סולוג, לי קופר, ניין ווסט, איזי ספיריט, אן קליין ושואו-אופ. עד 30.6.18, כפוף לתנאי המבצעים, להסרה השב הסר";
+      console.log(message);
+
+
+
+            //Send sms
+
+                      var message= req.body.FirstName+", תודה על הרשמתך !! קוד הטבת 10% הנחה: ("+result.coupon+"), למימוש חד פעמי באחת מהרשתות: גלי, סולוג, לי קופר, ניין ווסט, איזי ספיריט, אן קליין ושואו-אופ. עד 30.6.18, כפוף לתנאי המבצעים, להסרה השב הסר"
+                      //  var message = req.body.FirstName+", תודה על הרשמתך !! קוד הטבת 10% הנחה: ( "+couponId+" ), למימוש חד פעמי באחת מהרשתות: גלי, סולוג, לי קופר, ניין ווסט, איזי ספיריט, אן קליין, כל נעל סנטר ושואו-אופ. עד 30.5.18, כפוף לתנאי המבצעים, להסרה השב הסר";
+                        console.log("New Message"+message);
+
+
+
+
+                                request.post({url:'http://www.pages02.net/nessatltd-brill/iframeWS', form: {fn:req.body.FirstName,ln:req.body.LastName,phone:req.body.telephone,email:req.body.email,nl:"1",cpn:result.coupon,med:"sms",src:'קמפיין השקה'}}, function(err,httpResponse,body){ 
+              	console.log("Sent to watson "+httpResponse)
+              	//console.log(body)
+              })
