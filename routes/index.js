b57305d50744f3977404eafb159ee4a226686271
Razu Ahammed <razuahammed@Razus-iMac.local>
2018-05-27 18:52:22 +0530

    email function has beend fixed

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var mongodb = require("mongodb");
-var nodemailer = require('nodemailer');
-var hbs=require("nodemailer-express-handlebars")
 
 var MongoClient = require('mongodb').MongoClient;
 // var url = "mongodb://localhost:27017/liron";
@@ -11,24 +9,6 @@ var url = "mongodb://razu:munna707@ds239439.mlab.com:39439/liron"
 
 var request = require("request");
 
-
-
-// Email Setup
-
-
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'razuahammed.lpu@gmail.com',
-    pass: '^Munna^%707'
-  }
-});
-
-
-transporter.use('compile',hbs({
-	viewPath: 'views',
-	extName:".hbs"
-}));
 
 
 ///email
@@ -149,6 +129,17 @@ router.post('/', function(req, res, next) {
                           });
 
 
+                          console.log("Coupon sent to the user"+req.body.email );
+
+                          var email = req.body.email;
+                          var fn = req.body.FirstName;
+                          var ln = req.body.LastName;
+                          var phone = req.body.telephone;
+
+                          //request.post('http://www.pages02.net/nessatltd-brill/Razu/',{"form":{"fn":req.body.FirstName, "ln":req.body.LastName,"email":req.body.telephone,"phone":req.body.email,"coupon":couponId,"AD":1,"media":"sms","source":"קמפיין השקה"}})
+                          request.post('http://www.pages02.net/nessatltd-brill/Razu/',{"form":{"fn":fn, "ln":ln,"email":email,"phone":phone,"coupon":couponId,"AD":1,"media":"sms","source":"קמפיין השקה"}})
+
+
                         });
 
                         MongoClient.connect(url, function(err, db) {
@@ -167,25 +158,8 @@ router.post('/', function(req, res, next) {
                         console.log("Mail portion"+req.body.FirstName)
 
                       // send email
-                      var mailOptions = {
-                        from: 'noreply@stepin.co.il',
-                        to: req.body.email,
-                        subject:req.body.FirstName +",הטבת 10% הנחה, במיוחד בשבילך !! פרסומת",
-                        template: "email",
-                        context: {
-                        	coupon:couponId,                        }
-                      };
 
-
-                      transporter.sendMail(mailOptions, function(error, info){
-                        if (error) {
-                          console.log(error);
-                        } else {
-                          console.log('Email sent: ' + info.response);
-                        }
-                      });
-
-
+                      //request.post('http://www.pages02.net/nessatltd-brill/Razu/',{"form":{"fn":req.body.FirstName, "ln":req.body.LastName,"email":req.body.telephone,"phone":req.body.email,"coupon":couponId,"AD":1,"media":"sms","source":"קמפיין השקה"}})
 
 
 
@@ -199,22 +173,24 @@ router.post('/', function(req, res, next) {
 
 
 
-
                         var data = '<?xml version="1.0" encoding="UTF-8"?><sms><account><id>gali85</id> <password>gali713</password></account> <attributes><reference>123</reference><replyPath>0508085969</replyPath> </attributes><schedule> <relative>0</relative></schedule> <targets><cellphone reference="3542">'+userPhone+'</cellphone> </targets><data>'+message+'</data></sms>'
 
 
 
                         request.post(
-                      {url:'http://api.soprano.co.il/',
-                      body : data,
-                      headers: {'Content-Type': 'text/xml'}
-                      },
-                      function (error, response, body) {
-                          if (!error && response.statusCode == 200) {
-                              console.log(body)
+                          {url:'http://api.soprano.co.il/',
+                          body : data,
+                          headers: {'Content-Type': 'text/xml'}
+                          },
+                          function (error, response, body) {
+                              if (!error && response.statusCode == 200) {
+                                  console.log(body)
+                              }
                           }
-                      }
-                  );
+                      );
+
+
+
 
 
 
